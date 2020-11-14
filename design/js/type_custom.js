fdb4be5b7d5ff3c9c2780094c5141b251cbf1ae4
MohamedAnwerEida <01200643888r@gmail.com>
2020-11-14 08:10:47 +0200

    fix delete design folder

--- design/js/type_custom.js
+++
@@ -1,7 +0,0 @@
-// Start Typed js
-var typed = new Typed(".type", {
-  strings: ["AHMED MOHAMED Ali"],
-  typeSpeed: 90,
-  backSpeed: 90,
-  loop: true,
-});
--- design/js/type_custom_ar.js
+++
@@ -1,7 +0,0 @@
-// Start Typed js
-var typed = new Typed(".type", {
-  strings: ["محمود على حسن"],
-  typeSpeed: 90,
-  backSpeed: 90,
-  loop: true,
-});
